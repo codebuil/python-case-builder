b51110379cd20e96c5808f735413abd52059a665
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2022-11-11 19:06:00 +0000

    Add files via upload

---
+++ case.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+n=0;
+nn=0;
+print("\033c\033[42;30m\n");
+print("many cases ?\n");
+n=input("")
+print();
+print ("switch var1{");
+for nn in range (0,n):
+	print ("\tcase "+str(nn)+":");
+	print ("\t//code");
+print"\tdefault:\n//\n}\n"
+
+
